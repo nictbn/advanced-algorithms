75488e55d7614fdc26c8ec03c7daf50bc1aedb58
Nicu Taban <nictbndeveloper@gmail.com>
2023-06-01 14:34:26 +0300

    Implement Knuth Morris Pratt

--- src/main/java/knuth_morris_pratt/KnuthMorrisPratt.java
+++ src/main/java/knuth_morris_pratt/KnuthMorrisPratt.java
@@ -1,6 +1,23 @@
 package knuth_morris_pratt;
 
 public class KnuthMorrisPratt {
+
+    public int search(char[] array, char[] pattern) {
+        int[] lsp = computeLongestSuffixPrefixTable(pattern);
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            while (j > 0 && array[i] == pattern[j]) {
+                j = lsp[j - 1];
+            }
+            if (array[i] == pattern[j]) {
+                j++;
+                if (j == pattern.length) {
+                    return i - (j - 1);
+                }
+            }
+        }
+        return -1;
+    }
     public int[] computeLongestSuffixPrefixTable(char[] pattern) {
         int[] lsp = new int[pattern.length];
         int j = 0;
