177063a5724756b6bbd9e269c901a1657e7608ce
Nicu Taban <nictbndeveloper@gmail.com>
2023-06-03 17:26:10 +0300

    Boyer Moore End

--- src/main/java/boyer_moore/BoyerMoore.java
+++ src/main/java/boyer_moore/BoyerMoore.java
@@ -1,6 +1,65 @@
 package boyer_moore;
 
 public class BoyerMoore {
+    private final int ASCII_TABLE_SIZE = 256;
+
+    public int search(char[] array, char[] pattern) {
+        if (pattern == null || pattern.length == 0) {
+            return 0;
+        }
+        if (array == null) {
+            return -1;
+        }
+        int[] badCharacterTable = preComputeBadCharacterTable(pattern);
+        int[] suffixTable = preComputeSuffixTable(pattern);
+        for (int i = pattern.length - 1, j; i < array.length;) {
+            for (j = pattern.length - 1; pattern[j] == array[i]; i--, j--) {
+                if (j == 0) {
+                    return i;
+                }
+            }
+            i += Math.max(suffixTable[pattern.length - 1 - j], badCharacterTable[array[i]]);
+        }
+        return -1;
+    }
+
+    public int[] preComputeSuffixTable(char[] pattern) {
+        int[] table = new int[pattern.length];
+        computePrefix(pattern, table);
+        computeSuffix(pattern, table);
+        return table;
+    }
+
+    public int[] preComputeBadCharacterTable(char[] pattern) {
+        int[] table = new int[ASCII_TABLE_SIZE];
+        for (int i = 0; i < ASCII_TABLE_SIZE; i++) {
+            table[i] = pattern.length;
+        }
+        for (int t = 0; t < pattern.length - 1; t++) {
+            table[pattern[t]] = Math.max(1, pattern.length - t - 1);
+        }
+        if (table[pattern[pattern.length - 1]] < pattern.length) {
+            table[pattern[pattern.length - 1]] = 1;
+        }
+        return table;
+    }
+
+    public void computePrefix(char[] pattern, int[] table) {
+        int lastPrefixPosition = pattern.length;
+        for (int i = pattern.length; i > 0; i--) {
+            if (isPrefix(pattern, i)) {
+                lastPrefixPosition = i;
+            }
+            table[pattern.length - i] = lastPrefixPosition - i + pattern.length;
+        }
+    }
+
+    public void computeSuffix(char[] pattern, int[] table) {
+        for (int i = 0; i < pattern.length - 1; i++) {
+            int length = suffixLength(pattern, i);
+            table[length] = pattern.length - 1 - i + length;
+        }
+    }
     public boolean isPrefix(char[] pattern, int index) {
         for (int i = index, j = 0; i < pattern.length; i++, j++) {
             if (pattern[i] != pattern[j]) {
--- src/test/java/boyer_moore/BoyerMooreTest.java
+++ src/test/java/boyer_moore/BoyerMooreTest.java
@@ -9,6 +9,7 @@ class BoyerMooreTest {
     public static final char[] PATTERN_1 = "baibai".toCharArray();
     public static final char[] PATTERN_2 = "baidai".toCharArray();
     BoyerMoore b;
+
     @BeforeEach
     void setUp() {
         b = new BoyerMoore();
@@ -37,5 +38,39 @@ class BoyerMooreTest {
         assertEquals(3, b.suffixLength("kaikai".toCharArray(), 2));
         assertEquals(1, b.suffixLength("mytestmytest".toCharArray(), 2));
         assertEquals(6, b.suffixLength("mytestmytest".toCharArray(), 5));
+    }
+
+    @Test
+    public void computePrefixTest() {
+        int[] table = new int[6];
+        b.computePrefix(PATTERN_2, table);
+        assertArrayEquals(new int[]{6, 7, 8, 9, 10, 11}, table);
+    }
+
+    @Test
+    public void computeSuffixTEst() {
+        int[] table = new int[6];
+        b.computeSuffix(PATTERN_2, table);
+        assertArrayEquals(new int[]{1, 0, 5, 0, 0, 0}, table);
+    }
+
+    @Test
+    public void preprocessSuffixTableTest() {
+        int[] table = b.preComputeSuffixTable(PATTERN_2);
+        assertArrayEquals(new int[]{1, 7, 5, 9, 10, 11}, table);
+    }
+    @Test
+    public void searchTest() {
+        assertEquals(14, b.search("thisisour firsttest".toCharArray(), "tt".toCharArray()));
+        assertEquals(6, b.search("abcdbabaidai".toCharArray(), "baidai".toCharArray()));
+        assertEquals(0, b.search("abcdbabaidai".toCharArray(), "a".toCharArray()));
+        assertEquals(0, b.search("abcdbabaidai".toCharArray(), "ab".toCharArray()));
+        assertEquals(1, b.search("abcdbabaidai".toCharArray(), "b".toCharArray()));
+        assertEquals(9, b.search("abcdbabaidai".toCharArray(), "dai".toCharArray()));
+        assertEquals(3, b.search("asdz".toCharArray(), "z".toCharArray()));
+        assertEquals(0, b.search("asdz".toCharArray(), "".toCharArray()));
+        assertEquals(0, b.search("asdz".toCharArray(), null));
+        assertEquals(-1, b.search(null, "z".toCharArray()));
+        assertEquals(-1, b.search("abcdbabaidai".toCharArray(), "baidaiz".toCharArray()));
     }
 }
